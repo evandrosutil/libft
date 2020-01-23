a0e651ebde9a964bb6fa6494fbc4d97a69886ec7
Evandro De nadai sutil <ede-nada@c2r11p4.42sp.org.br>
2020-01-23 21:40:18 -0200

    cria ft_strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,7 +1,33 @@
-#include <stdio.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ede-nada <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/23 21:12:47 by ede-nada          #+#    #+#             */
+/*   Updated: 2020/01/23 21:34:43 by ede-nada         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main()
+char	*ft_strnstr(char *str, char *to_find, unsigned long int len)
 {
-    printf("%s\n", strnstr("eu quero pastel", "quero", 7));
+	unsigned long int	i;
+	unsigned long int	j;
+
+	i = 0;
+	if (to_find[0] == '\0')
+		return (str);
+	while (str[i] != 0 && i < len)
+	{
+		j = 0;
+		while (str[i + j] != '\0' && str[i + j] == to_find[j])
+		{
+			if (to_find[j + 1] == '\0')
+				return (&str[i]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
 }
