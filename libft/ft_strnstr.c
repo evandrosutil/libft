7422bee07d939b5098d1f5932edd159322718f05
Evandro de Nadai Sutil <evandrosutil@Evandros-MBP.lan>
2020-01-22 23:04:09 -0300

    inicial ft_strnstr e atualização da main

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    printf("%s\n", strnstr("eu quero pastel", "quero", 7));
+}
--- libft/main.c
+++ libft/main.c
@@ -13,16 +13,33 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
+#include <string.h>
 
-int			ft_toupper(int c);
+int         ft_atoi(const char *str);
+int         ft_isalnum(int c);
+int         ft_isalpha(int c);
+int         ft_isascii(int c);
+int         ft_isdigit(int c);
 int			ft_isprint(int c);
 long int	ft_strlen(const char *str);
+int         ft_strncmp(const char *s1, const char *s2, size_t n);
 int			ft_tolower(int c);
+int			ft_toupper(int c);
 
-int	main()
+int	main(int argc, char **argv)
 {
-	printf("ft_strlen: %lu // strlen: %lu\n", ft_strlen("pizza"), strlen("pizza"));
-	printf("ft_tolower: %c // tolower: %c\n", ft_tolower('C'), tolower('C'));
-	printf("ft_toupper: %c // toupper: %c\n", ft_toupper('c'), toupper('c'));
-	printf("ft_isprint: %d // isprint: %d\n", ft_isprint(33), isprint(33));
+    (void)argc;
+    if (argv[1])
+        printf("entrada: %s => ft_atoi: %d // atoi: %d\n", argv[1], ft_atoi(argv[1]), atoi(argv[1]));
+    printf("letra: %c => ft_isalpha: %d // isalpha: %d\n", '2', ft_isalpha('2'), isalpha('2'));
+    printf("digito: %c => ft_isdigit %d // isdigit: %d\n", 'a', ft_isdigit('a'), isdigit('a'));
+    printf("caractere: %c => ft_isalnum: %d // isalnum: %d\n", 122,  ft_isalnum(122), isalnum(122));
+    printf("caractere: %c => ft_isascii: %d // isascii: %d\n", 128, ft_isascii(128), isascii(128));
+    printf("string: %s => ft_strlen: %lu // strlen: %lu\n", "pizza",  ft_strlen("pizza"), strlen("pizza"));
+    printf("string: %s => ft_strncmp: %d // strncmp: %d\n", "lala", ft_strncmp("tudo", "tudo", 2), strncmp("tudo", "tudo", 2));
+    printf("string: %s => ft_strnstr: %s // strnstr: %s\n", "eu quero pastel => quero pastel", ft_strnstr("eu quero pastel", "quero", 7), strnstr("eu quero pastel", "quero", 7));
+    printf("caractere: %c => ft_tolower: %c // tolower: %c\n", 'C', ft_tolower('C'), tolower('C'));
+    printf("caractere: %c => ft_toupper: %c // toupper: %c\n", 'c', ft_toupper('c'), toupper('c'));
+    printf("ft_isprint: %d // isprint: %d\n", ft_isprint(33), isprint(33));
 }
