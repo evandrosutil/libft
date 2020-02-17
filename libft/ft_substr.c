33c43f10369280cf93f0ea6b4a2364dc9f1a18f4
Evandro De nadai sutil <ede-nada@c1r10p2.42sp.org.br>
2020-02-17 19:46:32 -0300

    ft_substr: fix start > str len

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,35 +6,29 @@
 /*   By: ede-nada <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 19:29:49 by ede-nada          #+#    #+#             */
-/*   Updated: 2020/02/06 19:27:07 by ede-nada         ###   ########.fr       */
+/*   Updated: 2020/02/17 19:45:49 by ede-nada         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, unsigned long int len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char				*str;
-	unsigned int		i;
-	unsigned int		j;
-	unsigned long int	l;
+	char			*str;
+	size_t			i;
+	size_t			l;
 
 	i = 0;
-	j = 0;
 	l = ft_strlen(s);
 	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
 		return (0);
 	if (!str)
 		return (0);
-	while (i < start)
-		i++;
-	while (j < len && s[i] != '\0')
+	while (i < len && (start + i) < l)
 	{
-		str[j] = s[i];
+		str[i] = s[i + start];
 		i++;
-		j++;
 	}
-	str[j] = '\0';
+	str[i] = '\0';
 	return (str);
 }
